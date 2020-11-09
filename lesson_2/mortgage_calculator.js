8afbc6837c483b63cc372707914b824a38a8ee5e
Chiara Fiorentini <chiara.fvw@gmail.com>
2020-11-09 11:52:26 +0100

    Corrections in morgage.

--- lesson_2/mortgage_calculator.js
+++ lesson_2/mortgage_calculator.js
@@ -5,49 +5,51 @@ function prompt(message) {
 }
 
 function invalidNumber(number) {
-  return number.trim() === '' ||
-  Number(number) <= 0 ||
-  Number.isNaN(Number(number));
+  return number <= 0 ||
+  Number.isNaN(number);
 }
 
 prompt("Welcome to the mortgage calculator!");
 let answer;
 do {
-  prompt("Please, insert the total loan amount.");
-  let loanAmount = readline.question();
+  prompt("Please, insert the total loan amount. Use only numbers.");
+  let loanAmount = Number(readline.question());
   while (invalidNumber(loanAmount)) {
     prompt("Please, insert a number.");
-    loanAmount = readline.question();
+    loanAmount = Number(readline.question());
   }
 
-  prompt("What is the Anual Percentage Rate?");
-  let apr = readline.question();
+  prompt("What is the Anual Percentage Rate? (Write 5 for 5%)");
+  let apr = Number(readline.question());
   while (invalidNumber(apr)) {
     prompt("Please, insert a number bigger than 0.");
-    apr = readline.question();
+    apr = Number(readline.question());
   }
 
-  prompt("How long will it take your loan (in years)?");
-  let loanDurationYears = readline.question();
-  while (invalidNumber(loanDurationYears)) {
-    prompt("Please, insert a number.");
-    loanDurationYears = readline.question();
+  prompt("How long will it take your loan? (Only full years)");
+  let loanDurationYears = Number(readline.question());
+  while (invalidNumber(loanDurationYears) || !Number.isInteger(loanDurationYears)) {
+    prompt("Please, insert a number. An integer number.");
+    loanDurationYears = Number(readline.question());
   }
 
   let loanDurationInMonths = loanDurationYears * 12;
   let monthlyInterestRate = (apr / 12) / 100;
-  let monthlyPayment = (Number(loanAmount) * 
+  let monthlyPayment = (loanAmount * 
                       (monthlyInterestRate / 
                       (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths)))))
                       .toFixed(2);
 
-  prompt(`You will have to pay $${monthlyPayment} a month.\n`);
+  prompt(`Having a total loan of ${loanAmount.toFixed(2)} to be covered in ${loanDurationYears} years with a ${apr}% APR, you will have to pay $${monthlyPayment} a month.\n`);
 
   prompt('Would you like to make another estimation?(y or n)');
   answer = readline.question().toLowerCase();
   while (answer !== 'n' && answer !== 'y') {
     prompt('Please, answer "y" or "n".');
     answer = readline.question().toLowerCase();
+  }
+  if (answer === 'n') {
+    prompt('Thank you for using the mortgage calculator. Bye!');
   }
 
 } while (answer === 'y');
